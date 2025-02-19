e83f768c20f79a3cf64adefabe13bedc6e78e7a9
vankevych <vankevychpetro@gmail.com>
2025-02-19 12:17:10 +0000

    and emails configuration and mailer

--- src/MessageHandler/PurchaseConfirmationNotificationHandler.php
+++ src/MessageHandler/PurchaseConfirmationNotificationHandler.php
@@ -3,15 +3,29 @@
 namespace App\MessageHandler;
 
 use App\Message\PurchaseConfirmationNotification;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Mime\Email;
 
 #[AsMessageHandler]
 class PurchaseConfirmationNotificationHandler
 {
+    public function __construct(private MailerInterface $mailer)
+    {
+    }
+
     public function __invoke(PurchaseConfirmationNotification $notification)
     {
         echo 'Creating a PDF contract note...<br>';
 
         echo 'Emailing contract note to ' . $notification->getOrder()->getBuyer()->getEmail() . '<br>';
+        $email = (new Email())
+            ->from('vankevychpetro@gmail.com')
+            ->to($notification->getOrder()->getBuyer()->getEmail())
+            ->subject('Contract note for order ' . $notification->getOrder()->getId())
+            ->text('Here is your contract note');
+
+        $this->mailer->send($email);
     }
 }
